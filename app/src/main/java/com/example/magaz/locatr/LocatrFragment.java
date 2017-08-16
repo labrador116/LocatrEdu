904c21c62bfc4e044ac12d041a4dd63fe09f2288
magaz <magazon4ik@yandex.ru>
2017-08-16 10:34:41 +0300

    changed

--- app/src/main/java/com/example/magaz/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/magaz/locatr/LocatrFragment.java
@@ -1,8 +1,17 @@
 package com.example.magaz.locatr;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,14 +21,21 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author Markin Andrey on 15.08.2017.
  */
 public class LocatrFragment extends Fragment {
+    public static final String TAG = "LocatrFragment";
     private ImageView mImageView;
     private GoogleApiClient mClient;
+    private ProgressDialog mProgressDialog;
 
     public static LocatrFragment newInstance() {
         Bundle args = new Bundle();
@@ -32,6 +48,9 @@ public class LocatrFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET},1);
+
         mClient = new GoogleApiClient.Builder(getActivity())
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
@@ -72,8 +91,77 @@ public class LocatrFragment extends Fragment {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_locate:
+                findImage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void findImage() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got a fix " + location);
+                        mProgressDialog = new ProgressDialog(getContext());
+                        mProgressDialog.setMessage("Загрузка картинки");
+                        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                        mProgressDialog.setIndeterminate(true);
+                        mProgressDialog.show();
+                        new SearchTask().execute(location);
+                    }
+                });
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+    }
+
+    @Override
     public void onStop() {
         super.onStop();
         mClient.disconnect();
+    }
+
+    private class SearchTask extends AsyncTask<Location, Void, Void>{
+        private GalleryItem mGalleryItem;
+        private Bitmap mBitmap;
+
+        @Override
+        protected Void doInBackground(Location... locations) {
+            FlickrFetchr fetchr = new FlickrFetchr();
+            List<GalleryItem> items = fetchr.searchPhotos(locations[0]);
+            if (items.isEmpty()){
+                return null;
+            }
+
+            mGalleryItem = items.get(0);
+
+            try {
+                byte[]bytes = fetchr.getUrlBytes(mGalleryItem.getUrl());
+                mBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            } catch (IOException e) {
+                Log.i(TAG, "Unable to download bitmap.", e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mImageView.setImageBitmap(mBitmap);
+            mProgressDialog.dismiss();
+        }
     }
 }
